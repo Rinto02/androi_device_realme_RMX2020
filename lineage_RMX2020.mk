9ee013676ecbb2274db832587674ebd16678be69
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-08-19 05:19:09 +0600

    RMX2020: RisingOS bringup

--- lineage_RMX2020.mk
+++ lineage_RMX2020.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common RisingOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_RMX2020
+PRODUCT_NAME := lineage_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2020
@@ -26,9 +26,20 @@ PRODUCT_SYSTEM_MODEL := RMX2020
 PRODUCT_SYSTEM_NAME := RMX2020
 PRODUCT_SYSTEM_DEVICE := RMX2020
 
+# RisingOS Stuffs
+RISING_BUILDTYPE := COMMUNITY
+WITH_GMS := false
+PRODUCT_NO_CAMERA := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_ENABLE_PIXEL_FEATURES := true
+TARGET_SUPPORTS_QUICK_TAP := false
+RISING_PACKAGE_TYPE := "VANILLA"
+
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME) \
+    RISING_CHIPSET="mt6768" \
+    RISING_MAINTAINER="Rinto"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
