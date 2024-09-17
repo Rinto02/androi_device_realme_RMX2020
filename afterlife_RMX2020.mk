480ea02f05ab3c644caf66e0246f3a5aeb34afae
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-09-17 15:30:09 +0600

    Update and rename lineage_RMX2020.mk to afterlife_RMX2020.mk
    Signed-off-by: 凛岡村リント <138510229+Rinto02@users.noreply.github.com>

--- afterlife_RMX2020.mk
+++ afterlife_RMX2020.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common RisingOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common afterlife stuff.
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX2020
+PRODUCT_NAME := afterlife_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2020
@@ -26,21 +26,15 @@ PRODUCT_SYSTEM_MODEL := RMX2020
 PRODUCT_SYSTEM_NAME := RMX2020
 PRODUCT_SYSTEM_DEVICE := RMX2020
 
-# RisingOS Stuffs
-RISING_BUILDTYPE := COMMUNITY
-WITH_GMS := true
-TARGET_CORE_GMS := true
-PRODUCT_NO_CAMERA := false
-TARGET_DEFAULT_PIXEL_LAUNCHER := false
-TARGET_ENABLE_PIXEL_FEATURES := true
+# afterlife Stuffs
+TARGET_CALL_RECORDING_SUPPORTED := true
+AFTERLIFE_ZIP_TYPE := CoreGApps
 TARGET_SUPPORTS_QUICK_TAP := false
-RISING_PACKAGE_TYPE := "GAPPS"
 
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME) \
-    RISING_CHIPSET="mt6768" \
-    RISING_MAINTAINER="Rinto"
+    ro.afterlife.maintainer=Rinto"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
